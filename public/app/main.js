36dd3879beca17c7ae3f11f87c97b4e70d902144
Didier Cabrera <didierctorres@gmail.com>
2015-04-22 17:10:29 -0600

    aa

--- public/app/main.js
+++ public/app/main.js
@@ -10,18 +10,21 @@ angular.module('devux',[])
 	$scope.reactions = [];
 
 	$http.get('/reactions').success(function(data){
-		$scope.reactions = data;
+		$scope.reactions = data.reverse();
 	});
 	$scope.addNew = function () {
 		$scope.container.adding = true;
 	}
 
 	$scope.submit = function (argument) {
-		console.log($scope.container);
 		$http.post('/reactions',{
 			imgsrc:$scope.container.src,
 			caption:$scope.container.caption
 		}).success(function (data) {
+			$scope.reactions.push({
+				imgsrc:$scope.container.src,
+				caption:$scope.container.caption				
+			});
 			$scope.container = {
 				adding:false,
 				src:'',
