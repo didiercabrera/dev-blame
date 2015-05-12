bf47a071842e8d1c8d04f70b212d06e3d3327022
Didier Cabrera <didierctorres@gmail.com>
2015-05-12 15:53:19 -0600

    fixes de like btns

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,18 +8,49 @@ angular.module('devux',[])
 	}
 
 	$scope.reactions = [];
-
+	$scope.tempData = [];
 	$http.get('/reactions/1').success(function(data){
 		$scope.reactions =[];
-		var order_data = data.sort(function (a,b){
-			return (a.likes||0) >(b.likes||0);
+		// var order_data = data.sort(function (a,b){
+		// 	var tms_1 = a._id.toString().substring(0,8);
+		// 	var tms_2 = b._id.toString().substring(0,8);
+
+		// 	var d1 = new Date( parseInt( tms_1, 16 ) * 1000 );
+		// 	var d2 = new Date( parseInt( tms_2, 16 ) * 1000 );
+		// 	return d1.getTime() > d2.getTime();
+		// });
+
+		var ts_data = data.map(function ( reaction ){
+			var time = reaction._id.toString().substring(0,8);
+			var date = new Date( parseInt( time, 16 ) * 1000 );
+			reaction.tms = date.getTime();
+			return reaction;
 		});
-		$scope.reactions = order_data.reverse();
+
+		$scope.reactions = ts_data;
+		tempData = ts_data;
 	});
 
 	$scope.addNew = function () {
 		$scope.container.adding = true;
 	}
+
+	$scope.all = function (){
+		$scope.reactions = tempData;
+	}
+
+	$scope.top = function (){
+		var top_data = [];
+		$scope.reactions.forEach(function (reaction){
+			if( reaction.likes && reaction.likes>5){
+				top_data.push(reaction);
+			}
+		});
+
+		tempData = $scope.reactions;
+		$scope.reactions = top_data;
+
+	};
 
 	$scope.submit = function (argument) {
 		$http.post('/reactions',{
